627baf9bdf6409fb3d062da7ea32329191b4f20c
ahmadalmuhtar <ahmadalmuhtar@gmail.com>
2024-01-29 23:39:01 +0100

    added a new address in the Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,4 @@
+// Header.jsx
 import React from "react";
 import "../styles/Header.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -19,21 +20,37 @@ const Header = () => {
         <h1>Mario Putzen</h1>
       </div>
       <div className="contact-info">
-        <div className="contact-item">
-          <FontAwesomeIcon icon={faPhone} className="contact-icon" />
-          <span className="contact-detail">+49 1520 2080771</span>
+        <div className="contact-set">
+          <div className="contact-item">
+            <FontAwesomeIcon icon={faPhone} className="contact-icon" />
+            <span className="contact-detail">+49 1520 2080771</span>
+          </div>
+          <div className="contact-item">
+            <FontAwesomeIcon icon={faEnvelope} className="contact-icon" />
+            <span className="contact-detail">Marioputzen25@gmail.com</span>
+          </div>
+          <div className="contact-item">
+            <FontAwesomeIcon icon={faMapMarkerAlt} className="contact-icon" />
+            <span className="contact-detail">
+              Labharswegstraße, 9. 78462 Konstanz
+            </span>
+          </div>
         </div>
-        <div className="contact-item">
-          <FontAwesomeIcon icon={faEnvelope} className="contact-icon" />
-          <span className="contact-detail">
-            Mail Us Marioputzen25@gmail.com
-          </span>
-        </div>
-        <div className="contact-item">
-          <FontAwesomeIcon icon={faMapMarkerAlt} className="contact-icon" />
-          <span className="contact-detail">
-            Labharswegstraße 9 78462 Konstanz
-          </span>
+        <div className="contact-set">
+          <div className="contact-item">
+            <FontAwesomeIcon icon={faPhone} className="contact-icon" />
+            <span className="contact-detail">+49 172 6228598</span>
+          </div>
+          <div className="contact-item">
+            <FontAwesomeIcon icon={faEnvelope} className="contact-icon" />
+            <span className="contact-detail">ahmadalmuhtar@gmail.com</span>
+          </div>
+          <div className="contact-item">
+            <FontAwesomeIcon icon={faMapMarkerAlt} className="contact-icon" />
+            <span className="contact-detail">
+              Frohnhauserstr, 308. 45144 Essen
+            </span>
+          </div>
         </div>
       </div>
     </div>
